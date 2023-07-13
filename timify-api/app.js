06a78e6d474fd166ba307cf9f2a5befe73daac77
Pearl Wogu <pearlw@meta.com>
2023-07-13 05:05:41 -0700

    Got a function that was able to make a HTTP request to my API. Deleted seeder files

--- timify-api/app.js
+++ timify-api/app.js
@@ -1,4 +1,5 @@
 import dotenv  from "dotenv"
+import axios from "axios";
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
@@ -8,89 +9,240 @@ dotenv.config()
 
 const app = express();
 
-
 app.use(cors())
 app.use(express.json()); 
 app.use(morgan('combined'))
 
-async function taddyGraphqlRequest ({query, variables}) {
-  const endpointURL = "https://api.taddy.org"
+const endpointURL = "https://api.taddy.org"
 
-  const headers = {
-    'Content-Type': 'application/json',
-    'User-Agent' : 'Timify',
-    'X-USER-ID': 484,
-    'X-API-KEY': "f0074dd996f94da9d99719653673f2d4cc35bacd7bc57d359daa48e35aeb80193bd2bc6d3df05aa388c4548856b9bb59c3",
-  }
+async function taddyGraphqlRequest(query, variables) {
+  const response = await axios({
+    url: endpointURL,
+    method: 'post',
+    data: {
+      query,
+      variables
+    },
+    headers : {
+      'Content-Type': 'application/json',
+      'User-Agent' : 'Timify',
+      'X-USER-ID': 484,
+      'X-API-KEY': `${process.env.API_KEY}`,
+    }
+  });
+  return response.data;
+}
 
-  try {
-    const client = new GraphQLClient(endpointURL, {headers})
-    const data = await client.request (query, variables)
-    return data;
-  }
-  catch (e) {
-    console.log("inside sentTaddyGraphqlRequest", e)
-  }
-
-  }
-
-  const GET_PODCASTSERIES = `
-  query getPodcastSeries($uuid: ID!) {
-    getPodcastSeries(uuid: $uuid){
-      uuid
-      name
-      imageUrl
-      description
-      authorName
-      datePublished
-      itunesId
-      genres
-      rssUrl
-      rssOwnerName
-      rssOwnerPublicEmail
-      hash
-      childrenHash
-      itunesInfo{
-        uuid
-        publisherId
-        publisherName
-        baseArtworkUrl
-        baseArtworkUrlOf(size: 640)
+export async function searchForTerm(term, page) {
+  const query = `
+    query SearchForTerm($term: String!, $page: Int!) {
+      searchForTerm(term: $term, page: $page) {
+        searchId
+        podcastSeries {
+          uuid
+          name
+          imageUrl
+          description
+          authorName
+          datePublished
+          description
+          imageUrl
+          genres
+          itunesInfo{
+            uuid
+            publisherId
+            publisherName
+            baseArtworkUrl
+            baseArtworkUrlOf(size: 640)
+          }
+        }
       }
     }
-  }
   `;
 
-  const GET_PODCASTEPISODE = `
-  query getPodcastEpisode($uuid: ID!) {
-    getPodcastEpisode(uuid: $uuid){
-      uuid
-      hash
-      name
-      description
-      imageUrl
-      datePublished
-      subtitle
-      audioUrl
-      fileLength
-      fileType
-      duration
-      episodeNumber
-      podcastSeries{
-        uuid
-        name
-        rssUrl
-        itunesId
-      }
-    }
-  }`;
+  const data = await taddyGraphqlRequest(query, { term, page });
+  return data.data.searchForTerm; // Adjust this line based on the actual structure of `data`
+}
+
+
+
+
+
+// const headers = {
+//   'Content-Type': 'application/json',
+//   'User-Agent' : 'Timify',
+//   'X-USER-ID': 484,
+//   'X-API-KEY': "f0074dd996f94da9d99719653673f2d4cc35bacd7bc57d359daa48e35aeb80193bd2bc6d3df05aa388c4548856b9bb59c3",
+// }
+
+//     export const fetchPodcastSeries = () => {
+//       const response = axios({
+//         url: endpointURL,
+//         method: "post",
+//         data: graphqlQuery,
+//         headers: headers
+//       });
+//       return response;
+//     };
+
+//     const SEARCH_FOR_TERM = `{
+//       query searchForTerm($term: String, $page: Int) {
+//         searchForTerm(term: $term, page: $page) {
+//           searchId
+//           podcastSeries {
+//             uuid
+//             name
+//             imageUrl
+//             description
+//             authorName
+//             datePublished
+//             description
+//             imageUrl
+//             genres
+//             itunesInfo{
+//               uuid
+//               publisherId
+//               publisherName
+//               baseArtworkUrl
+//               baseArtworkUrlOf(size: 640)
+//             }
+//           }
+//         }
+//       }
+      
+//     }`;
+
+//     const graphqlQuery = {
+//       operationName: "fetchPodcastSeries",
+//       query: `query fetchPodcastSeries ${SEARCH_FOR_TERM}`,
+//       variables: {}
+//     };
+
+
+//     fetchPodcastSeries()
+//     .then((res) => console.log("response", res.data)) // will return data object
+//     .catch((err) => console.log("err", err))
+    
+//     async function executeQuery(query, variables) {
+//       const response = await axios({
+//         url: TADDY_API_ENDPOINT,
+//         method: 'post',
+//         data: {
+//           query,
+//           variables
+//         },
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'Authorization': `Bearer ${process.env.TADDY_API_KEY}` // assuming you stored your API key in an environment variable
+//         }
+//       });
+//       return response.data;
+//     }
+    
+//     export async function getPodcastSeries(uuid) {
+//       const query = `query getPodcastSeries($uuid: ID!) {
+//         getPodcastSeries(uuid: $uuid){
+//           // Specify the data fields you need here...
+//         }
+//       }`;
+      
+//       const data = await executeQuery(query, { uuid });
+//       return data.data.getPodcastSeries; // Adjust this line based on the actual structure of `data`
+//     }
+    
+//     export async function getPodcastEpisode(uuid) {
+//       const query = `query getPodcastEpisode($uuid: ID!) {
+//         getPodcastEpisode(uuid: $uuid){
+//           // Specify the data fields you need here...
+//         }
+//       }`;
+    
+//       const data = await executeQuery(query, { uuid });
+//       return data.data.getPodcastEpisode; // Adjust this line based on the actual structure of `data`
+//     }
+    
+
+
 
   
- export {taddyGraphqlRequest}
- export const taddyQuery = {
-    GET_PODCASTSERIES,
-    GET_PODCASTEPISODE,
- };
+
+
+
+
+// podcastSeries
+
+    // async function taddyGraphqlRequest ({query, variables}) {
+  
+
+  
+//   try {
+//     const client = new GraphQLClient(endpointURL, {headers})
+//     const data = await client.request (query, variables)
+//     return data;
+//   }
+//   catch (e) {
+//     console.log("inside sentTaddyGraphqlRequest", e)
+//   }
+
+//   }
+
+//   const GET_PODCASTSERIES = `
+//   query getPodcastSeries($uuid: ID!) {
+//     getPodcastSeries(uuid: $uuid){
+//       uuid
+//       name
+//       imageUrl
+//       description
+//       authorName
+//       datePublished
+//       itunesId
+//       genres
+//       rssUrl
+//       rssOwnerName
+//       rssOwnerPublicEmail
+//       hash
+//       childrenHash
+//       itunesInfo{
+//         uuid
+//         publisherId
+//         publisherName
+//         baseArtworkUrl
+//         baseArtworkUrlOf(size: 640)
+//       }
+//     }
+//   }
+//   `;
+
+//   const GET_PODCASTEPISODE = `
+//   query getPodcastEpisode($uuid: ID!) {
+//     getPodcastEpisode(uuid: $uuid){
+//       uuid
+//       hash
+//       name
+//       description
+//       imageUrl
+//       datePublished
+//       subtitle
+//       audioUrl
+//       fileLength
+//       fileType
+//       duration
+//       episodeNumber
+//       podcastSeries{
+//         uuid
+//         name
+//         rssUrl
+//         itunesId
+//       }
+//     }
+//   }`;
+
+  
+//  export {taddyGraphqlRequest}
+//  export const taddyQuery = {
+//     GET_PODCASTSERIES,
+//     GET_PODCASTEPISODE,
+//  };
         
 
  
---
+++ timify-api/test.js
@@ -0,0 +1,17 @@
+import { searchForTerm } from './app.js'; // assuming taddyService.js is in the same directory
+
+// This is an async function so we can use await
+async function test() {
+  try {
+    // Replace "example term" with the term you want to search for, and 1 with the page number you want to fetch
+    const results = await searchForTerm("love", 1);
+
+    // This should log the results to your console
+    console.log(results);
+  } catch (error) {
+    // If there's an error, this will log it
+    console.error('An error occurred:', error);
+  }
+}
+
+test();
