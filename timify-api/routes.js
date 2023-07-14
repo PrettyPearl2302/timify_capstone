ef54ec6d4b443b9b909333493ee2a1f251857a17
Pearl Wogu <pearlw@meta.com>
2023-07-13 22:18:52 -0700

    write for loop to make function pass a different genre everytime

--- timify-api/routes.js
+++ timify-api/routes.js
@@ -10,18 +10,33 @@ app.use(express.json());
 
 // routes to get podcasts for home page display
 app.get('/api/home', async (req, res) => {
-    const term = ""; 
-    const page = 1;
-    const limitPerPage = 25;
-    const filterForGenres = ["PODCASTSERIES_BUSINESS"];
-  
-    try {
-      const podcasts = await searchForTerm(term, page, limitPerPage, filterForGenres);
-      res.json(podcasts);
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
-  });
+  const term = ""; 
+  const page = 1;
+  const limitPerPage = 10;
+  const Genres = ["PODCASTSERIES_BUSINESS", "PODCASTSERIES_COMEDY", "PODCASTSERIES_HEALTH_AND_FITNESS", "PODCASTSERIES_NEWS", "PODCASTSERIES_SOCIETY_AND_CULTURE", "PODCASTSERIES_SPORTS"]
+
+  let podcastByGenre = [];
+  let errorOccurred = null;
+
+  for (let Genre of Genres) {
+      try {
+          const podcasts = await searchForTerm(term, page, limitPerPage, [Genre]);
+          console.log(Genre, podcasts)
+          podcastByGenre.push(...podcasts.podcastSeries)
+      } catch (err) {
+          console.error(err);
+          if (!errorOccurred) {
+            errorOccurred = err;
+          }
+      }
+  }
+
+  if (errorOccurred) {
+    res.status(500).send(errorOccurred.message);
+  } else {
+    res.json(podcastByGenre);
+  }
+});
 
 
 app.listen(port, () => {
