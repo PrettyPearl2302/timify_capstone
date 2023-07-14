fadd30d0f2d9a9f913e7a9e61d8df9c179a8f92d
Pearl Wogu <pearlw@meta.com>
2023-07-13 22:19:25 -0700

    minor tweaks

--- timify-api/seedDatabase.js
+++ timify-api/seedDatabase.js
@@ -1,27 +1,32 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { Podcast } from './models/index.js';
+// import fs from 'fs';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+import { Podcast, Episode, ItunesInfo } from './models/index.js';
 import { sequelize } from './database.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { taddyGraphqlRequest , taddyQuery} from './app.js';
 
 
-const podcastData = JSON.parse(fs.readFileSync(path.resolve(__dirname, './seeders/podcasts.json'), 'utf8'));
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const seedDatabase = async () => {
-  try {
-    // Sync all models that aren't already in the database
-    await sequelize.sync({ alter: true });
 
-    await Podcast.bulkCreate(podcastData);
-    console.log('Podcast data has been seeded!');
-  } catch (error) {
-    console.error('Error seeding data:', error);
-  } finally {
-    await sequelize.close();
-  }
-};
 
-seedDatabase();
+
+
+// const podcastData = JSON.parse(fs.readFileSync(path.resolve(__dirname, './seeders/podcasts.json'), 'utf8'));
+
+// const seedDatabase = async () => {
+//   try {
+//     // Sync all models that aren't already in the database
+//     await sequelize.sync({ alter: true });
+
+//     await Podcast.bulkCreate(podcastData);
+//     console.log('Podcast data has been seeded!');
+//   } catch (error) {
+//     console.error('Error seeding data:', error);
+//   } finally {
+//     await sequelize.close();
+//   }
+// };
+
+// seedDatabase();
