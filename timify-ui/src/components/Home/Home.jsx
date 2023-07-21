fc0053b29e66d44935a7abb22eecebc90d7f56c1
Pearl Wogu <pearlw@meta.com>
2023-07-20 17:08:48 -0700

    this code works

--- timify-ui/src/components/Home/Home.jsx
+++ timify-ui/src/components/Home/Home.jsx
@@ -3,8 +3,6 @@ import { UserContext } from '../../state/UserContext.jsx';
 import "./Home.css"
 import Search from '../Search/Search';
 import PodcastGrid from '../PodcastGrid/PodcastGrid'
-import { Link } from 'react-router-dom';
-import { AiOutlineLoading } from 'react-icons/ai';
 
 const Home = () => {
   const {user, updateUser} = useContext(UserContext)
@@ -24,10 +22,9 @@ const Home = () => {
     fetchPodcasts();
   }, []);
 
+  
   const handleLogout = () => {
     updateUser(null)
-
-
   }
 
   return (
@@ -36,9 +33,7 @@ const Home = () => {
       <div className="user-info">
           {user ? (
             <>
-            <Link to="/my-profile/:id">
               <span>Hi, {user.username}! |</span>
-              </Link>
               <button onClick={handleLogout}>Logout</button>
             </>
           ) : (
