4168b8a226d7bd37b85a4d88b6c1eb7a9041ac45
Pearl Wogu <pearlw@meta.com>
2023-07-24 14:41:48 -0700

    created comemnt form successfully

--- timify-ui/src/components/AudioPlayer/AudioPlayer.jsx
+++ timify-ui/src/components/AudioPlayer/AudioPlayer.jsx
@@ -3,10 +3,26 @@ import "./AudioPlayer.css";
 
 const AudioPlayer = ({ audioUrl, fileType }) => {
   return (
+    <div>
     <div className="audio-wrapper">
       <audio controls>
         <source src={audioUrl} type={fileType} />
       </audio>
+    </div>
+
+    <div className="comment block">
+        <form className="new-comment-form">
+            <label>
+                Leave a comment:
+                <textarea 
+                    name="userComment"
+                    placeholder="Your comment goes here..."
+                    rows={4} cols={40} 
+                />
+            </label>
+            <button type="submit">Post Comment</button>
+        </form>
+    </div>
     </div>
   );
 };
