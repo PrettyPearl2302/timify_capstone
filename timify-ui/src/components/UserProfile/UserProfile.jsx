b326128c80cb336b3ceeaaa2f803151add840a6d
Pearl Wogu <pearlw@meta.com>
2023-07-25 10:13:10 -0700

    linked the two files

--- timify-ui/src/components/UserProfile/UserProfile.jsx
+++ timify-ui/src/components/UserProfile/UserProfile.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { UserContext } from '../../state/UserContext'
 import { useContext } from 'react'
+import './UserProfile.css'
 
 const UserProfile = () => {
   const {user, updateUser} = useContext(UserContext)
 
   return (
-    <>
+    <div className='user-profile'>
     <div>profile-picture</div>
     <div>username: {user.username}</div>
     <div>first name: {user.first_name}</div>
     <div>last name: {user.last_name}</div>
     <div>email address attached to this account: {user.email}</div>
-    </>
+    </div>
   )
 }
 
