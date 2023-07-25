dfd419da2079c4936eb10642d9d01696cf17d564
Pearl Wogu <pearlw@meta.com>
2023-07-25 11:18:01 -0700

    comments are getting into the database

--- timify-ui/src/components/EpisodeDetail/EpisodeDetail.jsx
+++ timify-ui/src/components/EpisodeDetail/EpisodeDetail.jsx
@@ -6,15 +6,16 @@ const EpisodeDetail = ({episode}) => {
 
   return (
     <>
-    <div onClick={() => (podcast)}></div>
+    <div onClick={() => (podcast)}>
     <div onClick={() => (episode)}>
       <div key={episode.uuid} className='episode-each'>
         <Link to={`/podcast/episode/${episode.uuid}`} style={{ textDecoration: "none", color: "inherit" }}>
-          <p className="episode-info">{episode.name}</p>
-        </Link>
+          <p className="episode-info">{episode.name}</p> //
+        </Link> 
         <p className="episode-info">{episode.episodeNumber}</p>
         <p className="episode-info">{episode.duration}</p>
       </div>
+    </div>
     </div>
     </>
     
