c1a3db94eb68aeb92058c1453afeeecea3997f08
Pearl Wogu <pearlw@meta.com>
2023-07-29 23:23:14 -0700

    minor tweaks

--- timify-ui/src/components/Rating/Rating.jsx
+++ timify-ui/src/components/Rating/Rating.jsx
@@ -36,9 +36,7 @@ const Rate = ({episodeId}) => {
 
 		if (hasRatedPreviously) {
 			alert(`You have previously rated this episode. Are you sure you want to update it to ${value} stars?`);
-		} else {
-			alert(`Are you sure you want to give ${value} stars?`);
-		}
+		} 
 
 		try {
 			const response = await fetch("http://localhost:3000/ratings", {
