6522eec9bf50503392f05e959ac25d831665fa07
Pearl Wogu <pearlw@meta.com>
2023-08-02 16:35:51 -0700

    added loading states across board

--- timify-ui/src/components/SearchResult/SearchResult.jsx
+++ timify-ui/src/components/SearchResult/SearchResult.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { AiOutlineLoading } from "react-icons/ai";
 import SearchResultCard from '../SearchResultCard/SearchResultCard';
 import './SearchResult.css';
 
@@ -8,20 +9,29 @@ import './SearchResult.css';
 const SearchResults = () => {
     const [podcastsByGenre, setPodcastsByGenre] = useState([]);
     const {term} = useParams();
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         const fetchPodcasts = async () => {
             try {
                 const response = await axios.get(`http://localhost:5000/api/search?term=${term}`);
                 setPodcastsByGenre(response.data.podcastSeries);
-                console.log(response)
+                setIsLoading(false)
             }
             catch (err) {
                 console.error(err);
+                setIsLoading(false)
             }
         };
         fetchPodcasts();
     }, [term]);
+
+    if (isLoading) {
+        return <div className='loading-state'>
+          <div><AiOutlineLoading /></div>
+          Loading...</div>
+      }
+
 
     return (
         <div className='all-podcast'>
