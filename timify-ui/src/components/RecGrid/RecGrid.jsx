b75b0137b46d7d76faec2f193bb9922c0b368014
Pearl Wogu <pearlw@meta.com>
2023-08-02 15:14:02 -0700

    successfully add a load more button for more  rec'd podcasts to load

--- timify-ui/src/components/RecGrid/RecGrid.jsx
+++ timify-ui/src/components/RecGrid/RecGrid.jsx
@@ -1,15 +1,23 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 import './RecGrid.css'
 import RecCard from '../RecCard/RecCard'
 
 const RecGrid = ({recPodcasts, refPodcastName}) => {
 
+    const [iterationIndex, setIterationIndex ] = useState(0)
+
+    const handleLoadMore = () => {
+        if (iterationIndex < recPodcasts.length - 1) {
+            setIterationIndex(iterationIndex + 1)
+        }
+    }
+
   return (
  
         <div>
-          {recPodcasts.map((podcastArray, index) => (
+          {recPodcasts.slice(0, iterationIndex + 1).map((podcastArray, index) => (
             <Fragment key={refPodcastName[index]}>
-              <h3 className='rec-heading'>Because you liked an episode from {refPodcastName[index]}</h3>
+              <h3 className='rec-heading'>Because you liked an episode from {refPodcastName[index]} :</h3>
               <div className='podcast-grid'>
                 {podcastArray.map((podcast) => (
                   <RecCard key={podcast.uuid} podcast={podcast} />
@@ -17,6 +25,11 @@ const RecGrid = ({recPodcasts, refPodcastName}) => {
               </div>
             </Fragment>
           ))}
+          {iterationIndex < recPodcasts.length - 1 ? (
+            <button onClick={handleLoadMore} className='load-more-btn'>Load More</button>
+          ) : (
+            <p>No more podcasts</p>
+          )} 
         </div>
 
 )
