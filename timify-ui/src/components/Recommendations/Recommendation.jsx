155265b8091383eca0f535736521473a12991e08
Pearl Wogu <pearlw@meta.com>
2023-08-01 23:16:44 -0700

    successfully pull recommended podcasts from database

--- timify-ui/src/components/Recommendations/Recommendation.jsx
+++ timify-ui/src/components/Recommendations/Recommendation.jsx
@@ -1,29 +1,46 @@
-import React, {useEffect, useContext} from 'react'
+import React, { useEffect, useContext, useState } from 'react';
 import { UserContext } from '../../state/UserContext.jsx';
-import "./Recommendation.css"
+import './Recommendation.css';
 
 const Recommendation = () => {
+  const user = useContext(UserContext);
+  const userId = user.user.id;
+  const [podcastGenre, setPodcastGenre] = useState([]);
 
-    const user = useContext(UserContext);
-    const userId = user.user.id; 
+  useEffect(() => {
+    const fetchRatingsbyValue = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/rec-ratings/${userId}`);
+        const data = await response.json();
+        const genres = data.map((index) => index.episode.podcast.genre); 
+        console.log(genres);
+        setPodcastGenre(genres);
+      } catch (error) {
+        console.error('Error while fetching ratings', error);
+      }
+    };
 
-    useEffect(() => {
-        const fetchRatingsbyValue = async () => {
-            try {
-                const response = await fetch(`http://localhost:3000/rec-ratings/${userId}`);
-                const data = await response.json();
-                console.log(data)
-            } catch (error) {
-                console.error("Error while fetching ratings", error)
-            }
+    fetchRatingsbyValue();
+  }, [userId]); 
+
+  useEffect(() => {
+    const fetchPodcastsByRec = async () => {
+      try {
+        for (const genre of podcastGenre) {
+          const response = await fetch(`http://localhost:5000/api/recommendations?Genre=${genre}`);
+          const data = await response.json();
+          console.log(data);
         }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+  
+    if (podcastGenre.length > 0) {
+      fetchPodcastsByRec();
+    }
+  }, [podcastGenre]); 
+  return <div>Recommendation</div>;
+};
 
-        fetchRatingsbyValue();
-    }, [])
-
-  return (
-    <div>Recommendation</div>
-  )
-}
-
-export default Recommendation
+export default Recommendation;
