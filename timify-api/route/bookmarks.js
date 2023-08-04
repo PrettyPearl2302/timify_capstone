2fb87b5c1eef06cd6b46ca48aded2f8a2694f534
Pearl Wogu <pearlw@meta.com>
2023-08-03 20:36:06 -0700

    create new route to post bookmarked podcasts into the database

---
+++ timify-api/route/bookmarks.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import { Bookmarked } from '../models/bookmarked'
+
+const router = express.Router()
+
+router.post('/bookmarks', async (req, res) => {
+  const { podcastId, authorName, name, genre, description, imageUrl, userId } = req.body
+  try {
+    const newBookmark = await Bookmarked.create({
+      podcastId,
+      authorName,
+      name,
+      genre,
+      description,
+      imageUrl,
+      userId
+    })
+    res.status(201).json({ bookmark: newBookmark })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Server error' })
+  }
+})
+
+export default router
