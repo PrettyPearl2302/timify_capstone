d6eec133b3758db458f077979581ee95627769ae
Pearl Wogu <pearlw@meta.com>
2023-08-03 20:22:53 -0700

    create data model for bookmarked

---
+++ timify-api/models/bookmarked.js
@@ -0,0 +1,33 @@
+import { DataTypes } from 'sequelize'
+import { sequelize } from '../database.js'
+
+export const Bookmarked = sequelize.define('Bookmarked', {
+  podcastId: {
+    type: DataTypes.UUID,
+    allowNull: false
+  },
+  authorName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  genre: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+})
